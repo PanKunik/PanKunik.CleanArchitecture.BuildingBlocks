6a7e3f34623beae9b3691ff85ba07920d19f5ad7
PanKunik <patryk.kunicki@pm.me>
2026-01-07 11:13:28 +0100

    Adding AggregateRoot

---
+++ src/CleanArchitecture.BuildingBlocks/AggregateRoot.cs
@@ -0,0 +1,27 @@
+﻿namespace PanKunik.CleanArchitecture.BuildingBlocks;
+
+public abstract class AggregateRoot<TId, TValue>
+    : Entity<TId, TValue>
+    where TId : EntityId<TValue>
+    where TValue : notnull
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        ArgumentNullException.ThrowIfNull(domainEvent);
+        _domainEvents.Add(domainEvent);
+    }
+
+    public IReadOnlyCollection<IDomainEvent> PullDomainEvents()
+    {
+        var events = _domainEvents.ToArray();
+        _domainEvents.Clear();
+        return events;
+    }
+
+    protected AggregateRoot(TId id)
+        : base(id) { }
+}
---
+++ src/CleanArchitecture.BuildingBlocks/IDomainEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace PanKunik.CleanArchitecture.BuildingBlocks;
+
+public interface IDomainEvent
+{
+    public DateTime OccuredOn { get; }
+}
